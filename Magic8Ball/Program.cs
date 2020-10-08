7bf3cc2fbbfeec4a9cd11234944cb798b2185b5c
Hughston Lockner <hughston94@gmail.com>
2020-10-08 08:50:00 -0400

    okmokm

--- Magic8Ball/Program.cs
+++ Magic8Ball/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Magic8Ball
 {
@@ -24,33 +25,90 @@ namespace Magic8Ball
         {
             while(true)
             {
+                int numberOfSecondsToSleep = randomObject.Next((5) + 1);
+                Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                 String questionString = GetQuestionFromUser();
-                int randomNumber = randomObject.Next(4);
+                Console.WriteLine("Hmmm... Let me think...");
+                Thread.Sleep(numberOfSecondsToSleep * 1000);
+                int randomNumber = randomObject.Next(10);
                 switch(randomNumber)
+
+
                 {
                     case 0:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("Yes!");
                             break;
                         }
                     case 1:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("No!");
                             break;
                         }
                     case 2:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("Bad Idea!");
                             break;
                         }
                     case 3:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("That's a mystery for the ages!");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("I would make doubts instead!");
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("your teacher is a ninja be nice!");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("42, is the answer to everything!");
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("stop asking questions and make me a pizza!");
+                            break;
+                        }
+                    case 8:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("maybe!");
+                            break;
+                        }
+                    case 9:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("this isnt Canada, speak english!");
                             break;
                         }
 
                 }
+                if(questionString.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                if(questionString.ToLower() == "ninja")
+                {
+                    Console.WriteLine("Shhh... we dont speak of the invisible");
+                    break;
+                }
             }
+
         }
 
     }
